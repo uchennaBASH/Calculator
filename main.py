c90feda6d45b139283d79fdd236c68858534e5dc
Uchenna Umeh <115172306+uchennaBASH@users.noreply.github.com>
2023-02-13 15:02:05 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+#Calculator
+
+num1 = float(input('Enter a number: '))
+op = input('Enter an operator: ')
+num2 = float(input('Enter another number: '))
+
+if op == '+':
+  print(num1 + num2)
+elif op == '-':
+  print(num1 - num2)
+elif op == '/':
+  print(num1 / num2)
+elif op == '*':
+  print(num1 * num2)
+else:
+  print('This operator is not supported. Please try again and input +, -, *, /')
